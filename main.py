f5e493e3ecfb3cc7c2dea484d3aad98f2b58a8c9
Nathan Fitzpatrick <Nathanfitz6@gmail.com>
2024-07-03 14:23:02 -0400

    fix: remove hjb import

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from tools.config import url, coin, ws_url, test_url, ws_test_url, coin_short
 from tools.utils import setup, elapsed_time
 from models.tokens import TokenInfo
 from data.trades import trade_sub
-from hjb_eq import hjb
 from hft import hft
-from avg_vol import avg_vol
 
 
 
